b8ce42a6610b6b1cb732b257824807900baa6eed
YasaminAbbaszadegan <Yasamin.abbaszadegan@ryerson.ca>
2024-05-16 17:04:42 -0400

    change name of app title

--- app.py
+++ app.py
@@ -1,52 +1,10 @@
-# import streamlit as st
-# import requests
-
-# # API_URL = ""
-# # API_TOKEN = ""
-# API_URL = "https://ry8lw5hyfuiiv805.us-east-1.aws.endpoints.huggingface.cloud"
-# API_TOKEN = "2gW0BQPEIHuZxNYGYZsmYNzRXXI_4hiDxccm7aMjMZPn8LLA6" 
-
-# headers = {"Authorization": f"Bearer {API_TOKEN}"}
-
-# def summarize_text(text):
-#     payload = {"inputs": text.strip()}
-#     response = requests.post(API_URL, json=payload, headers=headers)
-#     return response.json()
-
-# def main():
-#     st.title("Text Summarization App")
-    
-#     # Define a text area for user input
-#     text_input = st.text_area("Enter the text to summarize", height=300)
-
-
-#     # Add a button to trigger the summarization
-#     if st.button("Summarize"):
-#         # Display a spinner while summarization is in progress
-#         with st.spinner("Summarizing..."):
-#             # Call summarize_text function with user input
-#             summary = summarize_text(text_input)
-        
-#         # Display the summarized text in a print-like format
-#         st.subheader("Summary")
-#         st.markdown(f"```\n{summary}\n```")
-# if __name__ == "__main__":
-#     main()
-
-
 
 import streamlit as st
 import pandas as pd
 import requests
-# import locale
-# from statistics import mean
-# from evaluate import load
 
-
-
-
-API_URL = "https://ry8lw5hyfuiiv805.us-east-1.aws.endpoints.huggingface.cloud"
-API_TOKEN = "2gW0BQPEIHuZxNYGYZsmYNzRXXI_4hiDxccm7aMjMZPn8LLA6" 
+API_URL = ""
+API_TOKEN = ""
 
 # Load your dataframe
 data = pd.read_csv('sample_news.csv')
@@ -62,7 +20,7 @@ def summarize_text(text):
     return response.json()
 
 def main():
-    st.title("Text Summarization App")
+    st.title("News Summarization Genertor 📰 🗞️ ")
     
     # Option to select from dataframe
     use_dataframe = st.checkbox("Use dialogue from dataframe")
